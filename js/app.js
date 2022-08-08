c03ccea825d16d7cc0aad0a6d66b37edbc28df68
Nico R <ramireznicolas491@gmail.com>
2022-08-08 16:22:29 -0300

    Agregando swiper

--- js/app.js
+++ js/app.js
@@ -2,13 +2,27 @@ var swiper = new Swiper(".clientes__container--clientes", {
   speed: 1000,
   spaceBetween: 50,
   autoplay: {
-      delay: 2500,
+      delay: 2000,
       pauseOnMouseEnter: true,
     },
     loop: true,
   });
 
-  document.querySelector('.swiper').addEventListener('mouseleave', ()=>{
-
+  document.querySelector('.clientes__container--clientes').addEventListener('mouseleave', ()=>{
     swiper.autoplay.start();
+  })
+
+
+  var swiper2 = new Swiper(".swiper__texto", {
+    spaceBetween: 50,
+    speed: 1000,
+    autoplay: {
+      delay: 3000,
+    },
+    allowTouchMove: false,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   })
