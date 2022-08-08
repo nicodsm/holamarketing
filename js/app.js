c0f59d18de1041f132c6e53c342b3009053d95cb
Nico R <ramireznicolas491@gmail.com>
2022-08-08 11:08:28 -0300

    Agregando swiper

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 var swiper = new Swiper(".clientes__container--clientes", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
     autoplay: {
       delay: 2500,
       pauseOnMouseEnter: true,
