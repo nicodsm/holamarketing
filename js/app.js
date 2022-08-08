7a081bbc4a1390890011ed709a8ef98a09a68821
Nico R <ramireznicolas491@gmail.com>
2022-08-08 10:59:39 -0300

    Agregando swiper

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ var swiper = new Swiper(".clientes__container--clientes", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+      delay: 2500,
+      pauseOnMouseEnter: true,
+    },
+    loop: true,
   });
+
+  document.querySelector('.swiper').addEventListener('mouseleave', ()=>{
+
+    swiper.autoplay.start();
+  })
